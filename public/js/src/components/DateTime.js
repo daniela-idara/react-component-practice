59fd364f696299179d18ee00b2683ce589500d25
daniela-idara <purpleelephant1987@gmail.com>
2019-08-07 18:52:53 -0500

    missed a few props

--- public/js/src/components/DateTime.js
+++ public/js/src/components/DateTime.js
@@ -1,3 +1,3 @@
 function DateTime(props){
-    return( <p className="small text-muted"><i className="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>)
+    return( <p className="small text-muted"><i className="fa fa-clock-o"></i>{props.dateTime}</p>)
 }
